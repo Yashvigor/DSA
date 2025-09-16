d266e57a65736819b1f0262f85f45603b262be20
Yashvigor <yashvigor.ce@gmail.com>
2025-09-16 22:08:04 +0530

    Create removeElement.cpp

---
+++ LeetCode/removeElement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0; // Position to place the next non-val element
+
+    for (int i = 0; i < nums.size(); ++i) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+
+    }
+};
