10cf0516ca7b29ea21b30f0b7f929c0b26e4857f
Yashvigor <yashvigor.ce@gmail.com>
2025-09-16 22:18:15 +0530

    Create reverseVowels.cpp

---
+++ LeetCode/reverseVowels.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+string reverseVowels(string s) {
+    unordered_set<char> vowels = {'a','e','i','o','u','A','E','I','O','U'};
+    int left = 0, right = s.length() - 1;
+
+    while (left < right) {
+        // Move left pointer to the next vowel
+        while (left < right && vowels.find(s[left]) == vowels.end()) {
+            left++;
+        }
+        // Move right pointer to the previous vowel
+        while (left < right && vowels.find(s[right]) == vowels.end()) {
+            right--;
+        }
+
+        // Swap the vowels
+        swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+
+    return s;
+}
+
+
+};
