28d327058b427c1647b269224af17be93422bb59
Yashvigor <yashvigor.ce@gmail.com>
2025-09-26 19:21:12 +0530

    Create removeNodesLL.cpp

---
+++ LeetCode/removeNodesLL.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head) {
+            ListNode* nextNode = head->next;
+            head->next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+
+    ListNode* removeNodes(ListNode* head) {
+        head = reverseList(head);
+        int maxVal = head->val;
+        ListNode* curr = head;
+        while (curr && curr->next) {
+            if (curr->next->val < maxVal) {
+
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+                maxVal = curr->val;
+            }
+        }
+        return reverseList(head);
+    }
+};
