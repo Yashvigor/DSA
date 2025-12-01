411a1ae5c9ace3aa0f0d66dab90054c0b308a12d
Yashvi Gor <yashvigor.ce@gmail.com>
2025-12-01 20:21:46 +0530

    Add intersection function for two integer arrays
    Implement intersection of two arrays using a hashmap to track frequency.

---
+++ LeetCode/intersection2Arr-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> freq;
+        
+        for(int n : nums1) {
+            freq[n]++;
+        }
+        
+        vector<int> result;
+        
+        for(int n : nums2) {
+            if(freq[n] > 0) {
+                result.push_back(n);
+                freq[n]--;
+            }
+        }
+        
+        return result;
+    }
+};
